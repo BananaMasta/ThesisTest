5817d6288fd26778ba307b29775f82c71d849a3e
student01 <giopolizia@gmail.com>
2021-03-05 09:56:18 +0400

    добавление нового пункта

---
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -0,0 +1,35 @@
+-- begin THESISTEST_CUSTOMER
+create table THESISTEST_CUSTOMER (
+    ID uuid,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(100) not null,
+    EMAIL varchar(50) not null,
+    --
+    primary key (ID)
+)^
+-- end THESISTEST_CUSTOMER
+-- begin THESISTEST_ORDER
+create table THESISTEST_ORDER (
+    ID uuid,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    CUSTOMER_ID uuid,
+    DATE_ date not null,
+    AMOUNT decimal(19, 2),
+    --
+    primary key (ID)
+)^
+-- end THESISTEST_ORDER
