956cda4c9aae08c9677472897f30a5aaf1ea2d99
student01 <giopolizia@gmail.com>
2021-03-15 11:10:01 +0400

    2-3-6 осталось

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -40,3 +40,70 @@ alter table DF_CONTRACT add column CUSTOMER_ID uuid ^
 alter table DF_CONTRACT add column ORDER_ID uuid ^^-- end DF_CONTRACT
 --Modify Discriminator for df$Contract
 update WF_CARD set CARD_TYPE = 1120 where CARD_TYPE = 120^
+--Modify Discriminator for df$Employee
+update DF_CORRESPONDENT set TYPE = 'F' where TYPE = 'E'^
+-- begin DF_EMPLOYEE
+alter table DF_EMPLOYEE add column SIGNER_CATEGORY_ID uuid ^
+-- end DF_EMPLOYEE
+-- begin THESISTEST_CATEGORIES
+create table THESISTEST_CATEGORIES (
+    ID uuid,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    CODE varchar(50) not null,
+    NAME varchar(255) not null,
+    --
+    primary key (ID)
+)^
+INSERT INTO thesistest_categories
+(id, code,name)
+values(uuid_generate_v1(),'01', 'Нормативные и раcпорядительные документы Банка России'),(uuid_generate_v1(),'07', 'Переписка Банка с министерствами,ведомтсвами,предприятиями, учреждениями, клиентами и др'),(uuid_generate_v1(),'08', 'Переписка Банка с Банком России'), (uuid_generate_v1(),'09', 'Переписка Банка с налоговыми органами РФ')
+-- end THESISTEST_CATEGORIES
+-- begin DF_SIMPLE_DOC
+alter table DF_SIMPLE_DOC add column DOC_CARD_ID uuid ^
+-- end DF_SIMPLE_DOC
+--Modify Discriminator for df$SimpleDoc
+update WF_CARD set CARD_TYPE = 1110 where CARD_TYPE = 110^
+--Insert permissions for entity thesistest$Categories
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:create',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:update',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:delete',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:create',1,(select ID from SEC_ROLE where NAME = 'Administrators'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:update',1,(select ID from SEC_ROLE where NAME = 'Administrators'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:delete',1,(select ID from SEC_ROLE where NAME = 'Administrators'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:create',1,(select ID from SEC_ROLE where NAME = 'ReferenceEditor'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:update',1,(select ID from SEC_ROLE where NAME = 'ReferenceEditor'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Categories:delete',1,(select ID from SEC_ROLE where NAME = 'ReferenceEditor'));
+-- begin THESISTEST_SIGNERS
+create table THESISTEST_SIGNERS (
+    ID uuid,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME_ID uuid,
+    EMPLOYEE varchar(255),
+    --
+    primary key (ID)
+)^
+-- end THESISTEST_SIGNERS
+--Insert permissions for entity thesistest$Signers
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:create',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:update',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:delete',0,(select ID from SEC_ROLE where NAME = 'SimpleUser'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:create',1,(select ID from SEC_ROLE where NAME = 'Administrators'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:update',1,(select ID from SEC_ROLE where NAME = 'Administrators'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:delete',1,(select ID from SEC_ROLE where NAME = 'Administrators'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:create',1,(select ID from SEC_ROLE where NAME = 'ReferenceEditor'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:update',1,(select ID from SEC_ROLE where NAME = 'ReferenceEditor'));
+insert into SEC_PERMISSION (ID, CREATE_TS, CREATED_BY, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, PERMISSION_TYPE, TARGET, VALUE, ROLE_ID) values (newid(),now(),'system',1,now(),null,null,null,20,'thesistest$Signers:delete',1,(select ID from SEC_ROLE where NAME = 'ReferenceEditor'));
